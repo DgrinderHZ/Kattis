5314caeda3dd965bcefebaf2b85d19132b94df33
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-09 21:30:10 +0100

    Create Autori.cpp

---
+++ mcpc prep/Autori.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+ using namespace std;
+ int main(int argc, char const *argv[])
+ {
+    string str;
+    while( getline(cin,str,'-') ){
+      cout << str[0] ;
+    }
+      
+    return 0;
+ }  
