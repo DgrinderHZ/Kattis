670cfbcb351eb6cbfa5bc5bf07aafcb1303bbc7d
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-22 20:23:26 +0100

    Create R2.cpp

---
+++ R2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+ using namespace std;
+
+  
+ int main(int argc, char const *argv[])
+ {
+    int r,s;
+      cin >> r >> s;
+      
+        cout << (s * 2 - r) ;
+        
+    return 0;
+ }  
