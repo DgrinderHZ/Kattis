3f3cee49ce59916d91e67fa3cde3cc9881a545db
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-22 20:54:06 +0100

    Create Planina.cpp

---
+++ Planina.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin>>n;
+    n = pow(2,n) + 1;
+    n *= n;
+    cout << n;
+    return 0;
+}
