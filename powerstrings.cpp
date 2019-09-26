1e38bd45f2ed3c776ee3643d8d9bde5ad98505ce
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-26 21:57:02 +0100

    Create powerstrings.cpp

---
+++ powerstrings.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Macros
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define loop(i,a,b) for (int i = a; i < (b); i++)
+#define prec setprecision(n)
+#define endl '\n'
+
+//Short code
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int,int> pi;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int INF = numeric_limits<int>::max() - 9;
+
+int main ()
+{
+  #ifndef ONLINE_JUDGE
+  	//freopen ("input.txt","r",stdin);
+  	//freopen ("output.txt","w",stdout);
+  #endif
+
+  string s;
+  while(1){
+    cin >> s;
+    if (s == ".") break;
+    string an = "";
+    int larg = 0;
+    loop(i, 0, s.size()){
+        an += s[i];
+        string test(an);
+        int temp = 1;
+        while(test.size() < s.size()){
+            test += an;
+            temp++;
+        }
+
+        if(test == s && temp > larg) larg = temp;
+    }
+    cout << larg <<endl;
+  }
+
+
+  return 0;
+}
