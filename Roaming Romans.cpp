23b0023f882fac502662caf4219386504742812b
Hassan Zekkouri <32304244+DgrinderHZ@users.noreply.github.com>
2019-09-26 21:06:23 +0100

    Create Roaming Romans.cpp

---
+++ Roaming Romans.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Macros
+#define PI 3.14159265359
+#define all(v) v.begin(),v.end()
+#define sortva(v) sort(all(v))
+#define sortvd(v) sort(v.rbegin(),v.rend())
+#define sortaa(a,n) sort(a,a+n)
+#define sortad(a,n) sort(a,a+n),reverse(a,a+n)
+#define sfi1(v) scanf("%d",&v)
+#define sfi2(v1,v2) scanf("%d %d",&v1,&v2)
+#define sfi3(v1,v2,v3) scanf("%d %d %d",&v1,&v2,&v3)
+#define sfll1(v) scanf("%I64d",&v);
+#define sfll2(v1,v2) scanf("%I64d %I64d",&v1,&v2)
+#define sfll3(v1,v2,v3) scanf("%I64d %I64d %I64d",&v1,&v2,&v3)
+#define sfstr(v) scanf("%s", v);
+#define sz(v) (int)v.size()
+#define ndl puts("")
+#define SS stringstream
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define loop(i,a,b) for (int i = a; i < (b); i++)
+#define prec setprecision(n)
+#define endl '\n'
+
+//Short code
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef pair<int,int> pi;
+typedef unsigned long long ull;
+typedef long double ld;
+
+int INF = numeric_limits<int>::max() - 9;
+
+int main ()
+{
+  #ifndef ONLINE_JUDGE
+  	//freopen ("input.txt","r",stdin);
+  	//freopen ("output.txt","w",stdout);
+  #endif
+  float n;
+  cin >> n;
+
+  cout << (int)(n*1000.0*(5280.0/4854.0) + 0.5);
+
+
+  return 0;
+}
+
